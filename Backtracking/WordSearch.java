a4ac9f4b054030473a450ce6424e2ec27f0fdd69
shumin11 <shumin11@student.ubc.ca>
2025-01-08 01:19:43 -0800

    dfs

--- Backtracking/WordSearch.java
+++ Backtracking/WordSearch.java
@@ -2,41 +2,34 @@ import java.util.*;
 
 public class WordSearch{
     
-    private static int ROWS, COLS;
-    private static boolean[][] visited;
-
     public static boolean exist(char[][] board, String word) {
-        ROWS = board.length;
-        COLS = board[0].length;
-        visited = new boolean[ROWS][COLS];
-
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (dfs(board, word, r, c, 0)) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                boolean[][] visited = new boolean[m][n];
+                if (dfs(board, word, row, col, visited, 0)) {
                     return true;
                 }
             }
-        }
+        }  
         return false;
     }
 
-    private static boolean dfs(char[][] board, String word, int r, int c, int i) {
-        if (i == word.length()) {
+    private static boolean dfs(char[][] board, String word, int row, int col, boolean[][] visited, int index) {
+        if (index == word.length()) {
             return true;
         }
-
-        if (r < 0 || c < 0 || r >= ROWS || c >= COLS || 
-            board[r][c] != word.charAt(i) || visited[r][c]) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length ||
+            board[row][col] != word.charAt(index) || visited[row][col]) {
             return false;
         }
-
-        visited[r][c] = true;
-        boolean res = dfs(board, word, r + 1, c, i + 1) || 
-                      dfs(board, word, r - 1, c, i + 1) ||
-                      dfs(board, word, r, c + 1, i + 1) || 
-                      dfs(board, word, r, c - 1, i + 1);
-        visited[r][c] = false;
-
+        visited[row][col] = true;
+        boolean res = dfs(board, word, row + 1, col, visited, index + 1) ||
+                      dfs(board, word, row, col + 1, visited, index + 1) ||
+                      dfs(board, word, row - 1, col, visited, index + 1) ||
+                      dfs(board, word, row, col - 1, visited, index + 1);
+        visited[row][col] = false;
         return res;
     }
 
