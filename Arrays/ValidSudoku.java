dfd201e5f9ea547803690b74d920d6a9d6ad2a57
shumin11 <shumin11@student.ubc.ca>
2025-02-03 23:55:12 -0800

    alternative solution

--- Arrays/ValidSudoku.java
+++ Arrays/ValidSudoku.java
@@ -46,6 +46,32 @@ public class ValidSudoku {
         return true;
     }
 
+    // One Pass
+    public static boolean isValidSudoku2(char[][] board) {
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+
+                String squareKey = (r / 3) + "," + (c / 3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                    cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                    squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
+                    return false;
+                }
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         char[][] board = {
             { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
